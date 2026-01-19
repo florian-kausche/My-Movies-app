c078a207ba0511032c51c2939c2be23095fbdc3b
Florian Kausche <kau11006@byui.edu>
2026-01-19 12:41:20 +0000

    Florian Movie app

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ecf9d6d1e687687013947055c5177ceeb4c8e0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38ccee852d5031d87b67aa3a5cab53bfff94e12b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
